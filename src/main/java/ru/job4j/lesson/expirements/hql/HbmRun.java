c054a84c6f88a7bd1f2c5d8d6b8b4834b7b95ddf
RinZ26 <ivs1996@mail.ru>
2021-11-18 11:59:30 +0500

    0. HQL [#6874 #229581] fix cr

--- src/main/java/ru/job4j/lesson/expirements/hql/HbmRun.java
+++ src/main/java/ru/job4j/lesson/expirements/hql/HbmRun.java
@@ -6,6 +6,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.util.Collection;
 import java.util.TimeZone;
 
 public class HbmRun {
@@ -23,6 +24,7 @@ public class HbmRun {
             session.beginTransaction();
 
             Candidate alina = Candidate.of("alina", "20 years", 6666L);
+            Candidate fakeAlina = Candidate.of("alina", "33 years", 7777);
 
             Vacancy vacancyFirst = Vacancy.of("Java Dev");
             Vacancy vacancySecond = Vacancy.of("Spring Dev");
@@ -33,6 +35,7 @@ public class HbmRun {
             session.save(vacancySecond);
             session.save(successFullPeople);
             session.save(alina);
+            session.save(fakeAlina);
 
             alina.setVacancyRepo(successFullPeople);
 
@@ -42,9 +45,7 @@ public class HbmRun {
             result = (Candidate) session.createQuery("from Candidate c join fetch c.vacancyRepo vr join fetch vr.vacancies v where c.id = :id")
                     .setParameter("id", 1).uniqueResult();
 
-            session.createQuery("delete from Candidate c where c.id = :id")
-                    .setParameter("id", 1)
-                    .executeUpdate();
+            findCandidateByName(session, "alina").forEach(c -> System.out.println(c.getId() + "  " + c.getSalary()));
 
             session.getTransaction().commit();
             session.close();
@@ -54,5 +55,10 @@ public class HbmRun {
             StandardServiceRegistryBuilder.destroy(registry);
         }
         result.getVacancyRepo().getVacancies().forEach(v -> System.out.println(v.getName()));
+    }
+
+    public static Collection<Candidate> findCandidateByName(Session session, String name) {
+        return session.createQuery("from Candidate c where c.name = :name")
+                .setParameter("name", name).list();
     }
 }
